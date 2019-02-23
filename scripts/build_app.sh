b7a21da211ff563d9136dfa4da7b91cd89978550
Joseph Hughes <jjhughes57@gmail.com>
2019-02-23 15:33:51 -0500

    fix: move to known directory

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -33,4 +33,6 @@ done
 VERSION="$(node -p "require('./package.json').version")"
 NAME="$(node -p "require('./package.json').name" |  rev | cut -d/ -f1 | rev)"
 
-fpm -s dir -t deb -n "${NAME}" -v "${VERSION}" ./${PKG_FOLDER}/=/opt/mfp_cli/
+fpm -s dir -t deb -n "${NAME}" -v "${VERSION}" ./${PKG_FOLDER}/=/opt/${NAME}/
+mkdir -p deb_output 
+cp mv *.deb deb_output/ 
