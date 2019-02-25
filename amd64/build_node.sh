ee6d3b376c30af9ca44359b1ab035807e0788d57
Joseph Hughes <jjhughes57@gmail.com>
2019-02-25 11:06:26 -0500

    fix: target in bash script

--- amd64/build_node.sh
+++ amd64/build_node.sh
@@ -6,4 +6,4 @@ npm install
 
 export BUILD_CPUS=$(grep -c ^processor /proc/cpuinfo)
 
-node /builder/lib-es5/bin.js --targets node8-linux,node10-linux --build .
+node /builder/lib-es5/bin.js --targets node8-linux,node10-linux .
