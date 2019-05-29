762f7a63ed360f28cb6a17cad890bd0db68eaf30
Dan Winkler <dan@6river.com>
2019-05-28 23:31:25 -0400

    fix: Remove unused script.

--- amd64/build_node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-cd examples/express || exit 1
-
-npm install 
-
-export BUILD_CPUS=$(grep -c ^processor /proc/cpuinfo)
-
-node /builder/lib-es5/bin.js --targets node8-linux,node10-linux .
